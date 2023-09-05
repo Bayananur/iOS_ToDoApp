23c64fd85d177417b7886f2d44d2fae47f9f11ce
Bayana <bayananur@gmail.com>
2023-09-06 01:14:09 +0600

    add  func tableViewConfigure

--- iOS_ToDoApp/ToDo.swift
+++ iOS_ToDoApp/ToDo.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 struct ToDo: Codable {
     var title = ""
     var description = ""
--- iOS_ToDoApp/ToDoViewController.swift
+++ iOS_ToDoApp/ToDoViewController.swift
@@ -25,11 +25,37 @@ class ToDoViewController: UIViewController {
         super.viewDidLoad()
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         labelConfigure()
-       // tableViewConfigure()
-      //  newTaskButtonConstraints()
-      //  editButtonCostraints()
+        tableViewConfigure()
+       // newTaskButtonConstraints()
+       // editButtonCostraints()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        
+        do {
+            if let data = defaults.data(forKey: "taskItemArray") {
+                let array = try JSONDecoder().decode([ToDo].self, from: data)
+                self.array = array
+            }
+        } catch {
+            print("unable to encode \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func saveTasks() {
+        let defaults = UserDefaults.standard
+        do {
+            let encodedata = try JSONEncoder().encode(array)
+            defaults.set(encodedata, forKey: "taskItemArray")
+        } catch {
+            print("unable to encode \(error)")
+        }
+    }
+    
     func labelConfigure() {
         view.addSubview(label)
         
@@ -43,6 +69,20 @@ class ToDoViewController: UIViewController {
             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+    }
+    func tableViewConfigure() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
         ])
     }
     
